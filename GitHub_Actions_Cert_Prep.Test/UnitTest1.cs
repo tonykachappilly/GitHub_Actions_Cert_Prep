fae3458fa01db1682b0a6263486094988b0ea64d
tony_thomas <tonykachappilly@gmail.com>
2024-04-14 23:53:28 +0530

    added asp.net core web app and test

---
+++ GitHub_Actions_Cert_Prep.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace GitHub_Actions_Cert_Prep.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void My_First_Test()
+        {
+            Assert.Pass();
+        }
+    }
+}
