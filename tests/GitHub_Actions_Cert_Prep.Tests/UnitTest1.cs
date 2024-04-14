4392d8735c1caedf252ec6d490c7274fd8364083
tony_thomas <tonykachappilly@gmail.com>
2024-04-15 00:25:05 +0530

    changed to MS Tests

---
+++ tests/GitHub_Actions_Cert_Prep.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace GitHub_Actions_Cert_Prep.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void My_First_Test()
+        {
+        }
+    }
+}
