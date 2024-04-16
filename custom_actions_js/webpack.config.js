4ed0decb8f9396642661abc52bf472eba4d4c5b5
tony_thomas <tonykachappilly@gmail.com>
2024-04-16 16:09:51 +0530

    updated build

--- custom_actions_js/webpack.config.js
+++ custom_actions_js/webpack.config.js
@@ -4,10 +4,10 @@ const webpackNodeExternals = require('webpack-node-externals');
 module.exports = {
   target: 'node', // Compile for usage in a Node.js environment
   mode: 'production', // Set mode to production or development
-  entry: './src/main.js', // Entry point of your application
+  entry: './index.js', // Entry point of your application
   output: {
     path: path.resolve(__dirname, 'dist'), // Output directory
-    filename: 'main.bundle.js', // Output file name
+    filename: 'index.js', // Output file name
   },
   externals: [webpackNodeExternals()], // Exclude node_modules from the bundle
   module: {
