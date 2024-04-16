1cb3ccad304a877c8a8d29abf536e0bf0f25d159
tony_thomas <tonykachappilly@gmail.com>
2024-04-16 16:22:43 +0530

    created bundle using webpack

--- custom_actions_js/webpack.config.js
+++ custom_actions_js/webpack.config.js
@@ -1,27 +1,8 @@
-const path = require('path');
-const webpackNodeExternals = require('webpack-node-externals');
-
 module.exports = {
-  target: 'node', // Compile for usage in a Node.js environment
-  mode: 'production', // Set mode to production or development
-  entry: './index.js', // Entry point of your application
-  output: {
-    path: path.resolve(__dirname, 'dist'), // Output directory
-    filename: 'index.js', // Output file name
-  },
-  externals: [webpackNodeExternals()], // Exclude node_modules from the bundle
-  module: {
-    rules: [
-      {
-        test: /\.js$/, // Apply Babel to all .js files
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
-      },
-    ],
-  },
-};
+    entry: './src/index.js',
+    output: {
+      filename: 'index.js',
+      path: __dirname + '/dist'
+    },
+    target: 'node'
+  };
